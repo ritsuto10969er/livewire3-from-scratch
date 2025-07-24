e55da8dec1330b81acbcefc1b62baa3636a13685
ritsuto10969er <rk14oor109@gmail.com>
2025-07-24 17:50:39 +0900

    feat:Initial Livewire component and view setup

---
+++ app/Livewire/Greeter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Greeter extends Component
+{
+    public $name = "john";
+
+    public function render()
+    {
+        return view('livewire.greeter');
+    }
+}
