c771eb3654cdf845d5723e3c25fddec95df124b5
ritsuto10969er <rk14oor109@gmail.com>
2025-07-25 14:11:21 +0900

    Add validation with PHP 8 attributes for name field

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -3,14 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Greeter extends Component
 {
+    #[Validate('required|min:2')]
     public $name = "";
     public $greeting = '';
+    public $greetingMessage ='';
 
-    public function changeName($newName = '') {
+    public function changeGreeting() {
+        $this->reset('greetingMessage');
+
+        $this->validate();
+        
+        $this->greetingMessage = "{$this->greeting}, {$this->name}!";
     }
+
+    // public function rules () {
+    //     return [
+    //         'name' => 'required|min:2',
+    //     ];
+    // }
 
     public function render()
     {
